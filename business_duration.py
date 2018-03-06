55b7ef3749d77db2b797a4655e2a0c9c6b5db627
barrelman70 <azuehlke22@gmail.com>
2018-03-06 08:14:32 -0600

    update open and close start time
    update open and close start time when out of business hours to equal business start time.  It was showing as negative time is certain cases.

--- business_duration.py
+++ business_duration.py
@@ -47,10 +47,10 @@ def businessDuration(startdate,enddate,starttime=None,endtime=None,weekendlist=[
                 elif startdate.time() >= starttime and startdate.time() <= endtime: 
                     open_time = startdate.time()
                 else:
-                    open_time = time(0,0,0)
+                    open_time = starttime
                 #Calculate Closing day time in seconds
                 if enddate.time() < starttime:
-                    close_time = time(0,0,0)
+                    close_time = starttime
                 elif enddate.time() >= starttime and enddate.time() <= endtime:
                     close_time = enddate.time()
                 else:
